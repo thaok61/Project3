b824167f3a25be36f8e762464932acf60458b6ba
Thao <zztheblue@gmail.com>
2020-10-16 22:16:12 +0700

    update

--- project-3/src/components/HelloWorld.vue
+++ project-3/src/components/HelloWorld.vue
@@ -9,6 +9,10 @@
 </template>
 
 <script>
+  import CryptoJS from 'crypto-js'
+  const key = '2B7E151628AED2A6ABF7158809CF4F3C' // change to your key
+  const base64_iv = 'AAAAAAAAAAAAAAAAAAAAAA=='
+  const plain_iv = new Buffer(base64_iv, 'base64').toString('hex');
   export default {
     name: 'HelloWorld',
     mounted () {
@@ -16,24 +20,71 @@
     },
     data () {
       return {
-        temp:'',
-        humidity: ''
+        temp: this.aesDencrypt('KsqzbRL7DAaGuhH+GhJaNQ=='),
+        humidity: 0
       }
     },
     mqtt: {
       "esp8266/temperature" (data) {
-        this.temp = data
+        
+        // var stringData = new TextDecoder("utf-8").decode(data);
+        // var stringData = Buffer.from(data).toString('utf-8')
+        var stringData = this.utf8ArrayToString(data)
+        console.log(stringData);
+        this.temp = this.aesDencrypt(stringData)
       },
       "esp8266/humidity" (data) {
-        this.humidity = data
+        // var stringData = new TextDecoder("utf-8").decode(data);
+        var stringData = this.utf8ArrayToString(data)
+        this.humidity = this.aesDencrypt(stringData)
       }
     },
     methods: {
-      login() {
-        if (this.userName == this.$store.state.userName
-        && this.password == this.$store.state.password) {
-          this.$store.state.was_login = true
+      aesEncrypt(txt) {
+        const cipher = CryptoJS.AES.encrypt(txt, CryptoJS.enc.Hex.parse(key), {
+          iv: CryptoJS.enc.Hex.parse(plain_iv),
+          mode: CryptoJS.mode.CBC,
+          padding: CryptoJS.pad.ZeroPadding
+        })
+
+        return cipher
+      },
+      aesDencrypt(txt) {
+        const cipher = CryptoJS.AES.decrypt(txt, CryptoJS.enc.Hex.parse(key), {
+          iv: CryptoJS.enc.Hex.parse(plain_iv),
+          mode: CryptoJS.mode.CBC,
+          padding: CryptoJS.pad.ZeroPadding
+        })
+        var plaintext = cipher.toString(CryptoJS.enc.Base64);
+        var decoded_b64msg =  new Buffer(plaintext, 'base64').toString('ascii');
+        var decoded_msg =     new Buffer( decoded_b64msg , 'base64').toString('ascii')
+        return decoded_msg
+        // return cipher
+      },
+      utf8ArrayToString(aBytes) {
+        var sView = "";
+    
+        for (var nPart, nLen = aBytes.length, nIdx = 0; nIdx < nLen; nIdx++) {
+          nPart = aBytes[nIdx];
+        
+          sView += String.fromCharCode(
+            nPart > 251 && nPart < 254 && nIdx + 5 < nLen ? /* six bytes */
+                /* (nPart - 252 << 30) may be not so safe in ECMAScript! So...: */
+                (nPart - 252) * 1073741824 + (aBytes[++nIdx] - 128 << 24) + (aBytes[++nIdx] - 128 << 18) + (aBytes[++nIdx] - 128 << 12) + (aBytes[++nIdx] - 128 << 6) + aBytes[++nIdx] - 128
+            : nPart > 247 && nPart < 252 && nIdx + 4 < nLen ? /* five bytes */
+                (nPart - 248 << 24) + (aBytes[++nIdx] - 128 << 18) + (aBytes[++nIdx] - 128 << 12) + (aBytes[++nIdx] - 128 << 6) + aBytes[++nIdx] - 128
+            : nPart > 239 && nPart < 248 && nIdx + 3 < nLen ? /* four bytes */
+                (nPart - 240 << 18) + (aBytes[++nIdx] - 128 << 12) + (aBytes[++nIdx] - 128 << 6) + aBytes[++nIdx] - 128
+            : nPart > 223 && nPart < 240 && nIdx + 2 < nLen ? /* three bytes */
+                (nPart - 224 << 12) + (aBytes[++nIdx] - 128 << 6) + aBytes[++nIdx] - 128
+            : nPart > 191 && nPart < 224 && nIdx + 1 < nLen ? /* two bytes */
+                (nPart - 192 << 6) + aBytes[++nIdx] - 128
+            : /* nPart < 127 ? */ /* one byte */
+                nPart
+          );
         }
+    
+        return sView;
       }
     },
     computed: {
---
+++ publicDHTtoWeb/aes_encryption/codeDHT.ino
@@ -0,0 +1,143 @@
+// Loading the ESP8266WiFi library and the PubSubClient library
+#include <DHT.h>
+#include <DHT_U.h>
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+#include "AESLib.h"
+
+
+// define DHT 11
+#define DHTTYPE DHT11 //DHT 11
+
+//Change the credientials below, so your ESP8266connects to your router
+const char* ssid = "test";
+const char* password = "12345678";
+
+//Change the variable to yur Raspberry Pi IP address, so it connects to your MQTT broker
+const char* mqtt_server = "192.168.43.179";
+const char* mqtt_user = "thao";
+const char* mqtt_pass = "1";
+
+// Intializes the AESLib , Key, IV
+AESLib aesLib;
+
+byte key[] = { 0x2B, 0x7E, 0x15, 0x16, 0x28, 0xAE, 0xD2, 0xA6, 0xAB, 0xF7, 0x15, 0x88, 0x09, 0xCF, 0x4F, 0x3C };
+byte my_iv[N_BLOCK] = { 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+
+// Intializes the espClient
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+// DHT Sensor
+const int DHTPin = 4;
+
+// Initialize DHT Sensor.
+DHT dht(DHTPin, DHTTYPE);
+
+// Timer's auxiliary variables
+long now = millis();
+long lastMeasure = 0;
+
+// Don't change the function below. This functions connects your ESP8266 to your router
+void setup_wifi() {
+  delay(10);
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+    }
+  Serial.println("");
+  Serial.print("WiFi connected - ESP IP address: ");
+  Serial.println(WiFi.localIP());
+} 
+
+//This functions reconnects your ESP8266 to your MQTT broker
+// Change the function below if you want to subscribe to more topics with your ESP8266
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Attempt to connect
+
+    if (client.connect("ESP8266lights", mqtt_user, mqtt_pass)) {
+      Serial.println("connected");
+    } 
+    else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+} 
+
+//The setup function sets your ESP GPIOs to Outputs, starts the serial communication at 
+//a baud rate of 115200
+// Sets your mqtt broker and sets the callback function
+// The callback function is what receives messages and actually controls the LEDs
+void setup() {
+  dht.begin();
+  
+  Serial.begin(9600);
+  setup_wifi();
+  client.setServer(mqtt_server, 1883);
+  
+} 
+
+//For this project, you don't need to change anything in the loop function.
+// Basically it ensures that you ESP is connected to your broker
+void loop() {
+  if (!client.connected()) {
+    reconnect();
+  }
+  if (!client.loop())
+    client.connect("ESP8266lights", mqtt_user, mqtt_pass);
+  now = millis();
+  // Pulishes new temperature and humidity every 5 seonds
+  if (now - lastMeasure > 5000) {
+    lastMeasure = now;
+    // Read the humidity values
+    float h = dht.readHumidity();
+    // Read the temperature as Celsius (the default)
+    float t= dht.readTemperature();
+    // Checking the readings(to tru again)
+    if (isnan(h) || isnan(t)) {
+      Serial.println("Failed to read from DHT sensor");
+      return;
+    }
+    static char temperatureTemp[7];
+    dtostrf(t, 6, 2, temperatureTemp);
+
+    static char humidityTemp[7];
+    dtostrf(h, 6, 2, humidityTemp);
+    
+    String encHumidity = aesLib.encrypt(humidityTemp, key, my_iv);
+    String encTemperature = aesLib.encrypt(temperatureTemp, key, my_iv);
+    	
+
+    int str_LengthTemperature = encTemperature.length() + 1;
+    int str_LenghtHumidity = encHumidity.length() + 1;
+    
+    char encTemperatureChar[str_LengthTemperature];
+    char encHumidityChar[str_LenghtHumidity];
+
+    encTemperature.toCharArray(encTemperatureChar, str_LengthTemperature);
+    encHumidity.toCharArray(encHumidityChar, str_LenghtHumidity);
+    
+    // Publishes Temperature and Humidity values
+    client.publish("esp8266/temperature",encTemperatureChar);
+    client.publish("esp8266/humidity",encHumidityChar);
+
+    Serial.print("Humidity: ");
+    Serial.print(encHumidity);
+    
+    Serial.print(" Temperature: ");
+    Serial.print(encTemperature);
+    Serial.println(" \t\t\t\t\t *C ");
+  }
+}
